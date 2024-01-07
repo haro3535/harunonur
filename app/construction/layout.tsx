260ded2111f2dfb0c07bace363051f544ac55dc6
haro3535 <66617253+haro3535@users.noreply.github.com>
2024-01-07 16:36:37 +0300

    trials

--- app/construction/layout.tsx
+++ app/construction/layout.tsx
@@ -5,8 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: '503 | Harun Onur',
-  description: 
-      'Welcome to my personal webpage! I\'m a 3rd-year Computer Engineering student passionate about both frontend and backend development. Every day is an opportunity to learn and improve. Join me on this coding journey as I showcase my projects and continuous exploration in the tech world.',
+  description: '',
 }
 
 export default function RootLayout({
@@ -16,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   )
 }
--- app/construction/page.tsx
+++ app/construction/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Construction() {
     return (
-        <main className="container-fluid min-h-screen flex flex-col items-center justify-center">
+        <main className="container-fluid min-h-screen flex flex-col items-center justify-center bg-white">
             <p>Under Construction</p>
         </main>
     )
