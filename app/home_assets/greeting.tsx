661d85cabeb95404983f19b78d206ee25e840e75
haro3535 <66617253+haro3535@users.noreply.github.com>
2024-01-16 15:34:21 +0300

    Update greeting.tsx

--- app/home_assets/greeting.tsx
+++ app/home_assets/greeting.tsx
@@ -4,7 +4,7 @@ export default function Greeting(){
     return(
         <div className='w-full py-40 bg-blue-400'>
             <div className="w-full flex flex-col h-96 justify-center items-center text-6xl text-white">
-                <p className="">"Welcome to my webpage!"</p>
+                <p className="">Welcome to my webpage!</p>
             </div>
         </div>
     );
